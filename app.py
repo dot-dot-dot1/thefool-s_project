88f6e7629b51f9b2de3c43989282e11a443cf224
Mona Lisa <flame728xd@gmail.com>
2025-12-29 23:08:34 -0500

    Added login page to backend (2025-12-29)

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Welcome to our Flask Web Application"
+@app.route("/") #Used to redicrect user to login page when using website
+def home():
+    return redicrect(url_for("login"))
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+# LOGIN
+@app.route("/login", methods=["GET", "POST"]) #Offers 2 login methods
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Later: check database
+        print(email, password)
+
+        return "Login received"
+
+    return render_template("login.html")
+
+# SIGN UP
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Later: save to database
+        print(email, password)
+
+        return "Signup received"
+
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
